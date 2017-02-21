e08235d547e564aedf4089c3a57def6605a16893
andre <brandandre@freenet.de>
2017-02-21 12:50:06 +0100

    Dice ON Off

--- app/components/DiceBox/index.js
+++ app/components/DiceBox/index.js
@@ -5,20 +5,34 @@
 */
 
 import React, { PropTypes } from 'react';
-
+import styled from 'styled-components';
+import diceOn from '../../assets/DICES.png'
+import diceOff from '../../assets/Dicesoff.png'
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+const Dice = styled.div`
+    width: 10vh;
+    height: 10vh;
+    background-size: contain;
+    background-repeat:no-repeat;
+`;
+
+const DiceOn = styled(Dice)`
+    background-image: url(${diceOn});
+`;
+
+const DiceOff = styled(Dice)`
+    background-image: url(${diceOff});
+`;
 
 function DiceBox({ onRollTheDice, canRollTheDice, diceResult }) {
   return (
     <div>
       <FormattedMessage {...messages.header} />
       Dice result: [{diceResult}]
-      <button
-        disabled={!canRollTheDice}
-        onClick={onRollTheDice}
-      >Roll the dice</button>
+
+        {!canRollTheDice ? <DiceOff /> : <DiceOn onClick={onRollTheDice} />}
     </div>
   );
 }
